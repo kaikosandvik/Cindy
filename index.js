8ec7d1facc87821c6d3d715b7d79041ae7351c67
Cindy <kaikosandvik@gmail.com>
2018-06-13 23:19:57 +0800

    'ok'

--- index.js
+++ index.js
@@ -3,17 +3,14 @@ var express = require('express');
 
 var bot = linebot({
   channelId: '1586338101',
-  channelSecret: '9e60b5dab4058c2b2665648c9d8beeae',
+  channelSecret: 'c67cb682c2dc34cbd9d601fdbc6e7d6b',
   channelAccessToken: 'qSN3YcTHfx3vxESD+qaacqSUkUW+RmoY16kREXiILYrsOk5lXKUpuN7orpaYjFBPtYo205jffGk3q1TnO58hUP/Xepb/jNQ9kYRsYghw3a5qRFjhXcSMun+L55Ol6oWnujlP4yDQxGO1eTdSxQ+ycwdB04t89/1O/w1cDnyilFU='
 });
 
 //這一段的程式是專門處理當有人傳送文字訊息給LineBot時，我們的處理回應
 bot.on('message', function(event) {
   if (event.message.type = 'text') {
-    var {
-  "type": "text",
-  "text": "您好,\n請問需求類別?\n1.訂購-凱國\n2.訂購-鈿洋\n3.WTO詢價\n4.山特查價與交期\n5.打報價單\n6.登二級\n7.寄目錄\n8.其他,請自填"
-}
+    var msg = event.message.text;
   //收到文字訊息時，直接把收到的訊息傳回去
     event.reply(msg).then(function(data) {
       // 傳送訊息成功時，可在此寫程式碼 
@@ -31,5 +28,5 @@ app.post('/', linebotParser);
 
 var server = app.listen(process.env.PORT || 8080, function() {
   var port = server.address().port;
-  console.log('192.168.0.18', port);
+  console.log('目前的port是', port);
 });
